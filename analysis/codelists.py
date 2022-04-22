227fd365bd8d6522d5d7d9329e9ed94a804dee60
bangzheng2217 <b.zheng17@imperial.ac.uk>
2022-04-22 21:41:46 +0100

    Update codelists.py

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -6,7 +6,10 @@ from cohortextractor import (codelist, codelist_from_csv, combine_codelists)
 
 
 # --- CODELISTS ---
-
+#SUS-HES mabs
+mabs_procedure_codes = codelist(
+  ["X891", "X892"], system="opcs4", column="code"
+)
 # Chronic cardiac disease
 chronic_cardiac_dis_codes = codelist_from_csv(
     "codelists/opensafely-chronic-cardiac-disease-snomed.csv",
