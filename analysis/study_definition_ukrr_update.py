20b7e664156beb622f4a4b533ce18ff5fabc87a8
bangzheng2217 <b.zheng17@imperial.ac.uk>
2023-04-08 18:33:27 +0100

    Update study_definition_ukrr_update.py

--- analysis/study_definition_ukrr_update.py
+++ analysis/study_definition_ukrr_update.py
@@ -524,14 +524,28 @@ study = StudyDefinition(
     #with_admission_method=["21", "22", "23", "24", "25", "2A", "2B", "2C", "2D", "28"], # emergency admissions only to exclude incidental COVID
     on_or_before = "start_date - 1 day",
     date_format = "YYYY-MM-DD",
-    find_first_match_in_period = False,
+    find_last_match_in_period = True,
     return_expectations = {
       "date": {"earliest": "2021-12-20", "latest": "index_date - 1 day"},
       "rate": "uniform",
       "incidence": 0.05
     },
   ),
-  
+  primary_covid_hospital_admission_date = patients.admitted_to_hospital(
+    returning = "date_admitted",
+    with_these_primary_diagnoses = covid_icd10_codes,
+    with_patient_classification = ["1"], # ordinary admissions only - exclude day cases and regular attenders
+    # see https://docs.opensafely.org/study-def-variables/#sus for more info
+    #with_admission_method=["21", "22", "23", "24", "25", "2A", "2B", "2C", "2D", "28"], # emergency admissions only to exclude incidental COVID
+    on_or_before = "start_date - 1 day",
+    date_format = "YYYY-MM-DD",
+    find_last_match_in_period = True,
+    return_expectations = {
+      "date": {"earliest": "2021-12-20", "latest": "index_date - 1 day"},
+      "rate": "uniform",
+      "incidence": 0.05
+    },
+  ),  
   any_covid_hospital_discharge_date = patients.admitted_to_hospital(
     returning = "date_discharged",
     with_these_diagnoses = covid_icd10_codes,
@@ -540,7 +554,7 @@ study = StudyDefinition(
     with_admission_method=["21", "22", "23", "24", "25", "2A", "2B", "2C", "2D", "28"], # emergency admissions only to exclude incidental COVID
     on_or_before = "start_date - 1 day",
     date_format = "YYYY-MM-DD",
-    find_first_match_in_period = False,
+    find_last_match_in_period = True,
     return_expectations = {
       "date": {"earliest": "2021-12-20", "latest": "index_date - 1 day"},
       "rate": "uniform",
@@ -1412,10 +1426,11 @@ study = StudyDefinition(
   vaccination_status = patients.categorised_as(
     {
       "Un-vaccinated": "DEFAULT",
-      "Un-vaccinated (declined)": """ covid_vax_declined AND NOT (covid_vax_1 OR covid_vax_2 OR covid_vax_3)""",
+      "Un-vaccinated (declined)": """ covid_vax_declined AND NOT (covid_vax_1 OR covid_vax_2 OR covid_vax_3 OR covid_vax_4)""",
       "One vaccination": """ covid_vax_1 AND NOT covid_vax_2 """,
       "Two vaccinations": """ covid_vax_2 AND NOT covid_vax_3 """,
-      "Three or more vaccinations": """ covid_vax_3 """
+      "Three vaccinations": """ covid_vax_3 AND NOT covid_vax_4 """,
+      "Four or more vaccinations": """ covid_vax_4 """
     },
     
     # first vaccine from during trials and up to treatment/test date
@@ -1442,13 +1457,21 @@ study = StudyDefinition(
       returning = "date",
       date_format = "YYYY-MM-DD"
     ),
+    # add 4th
+    covid_vax_4 = patients.with_tpp_vaccination_record(
+      target_disease_matches = "SARS-2 CORONAVIRUS",
+      between = ["covid_vax_3 + 56 days", "start_date"],
+      find_first_match_in_period = True,
+      returning = "date",
+      date_format = "YYYY-MM-DD"
+    ),
 
     covid_vax_declined = patients.with_these_clinical_events(
       covid_vaccine_declined_codes,
       returning="binary_flag",
       on_or_before = "start_date",
     ),
-    
+
     return_expectations = {
       "rate": "universal",
       "category": {
@@ -1456,8 +1479,9 @@ study = StudyDefinition(
           "Un-vaccinated": 0.1,
           "Un-vaccinated (declined)": 0.1,
           "One vaccination": 0.1,
-          "Two vaccinations": 0.2,
-          "Three or more vaccinations": 0.5,
+          "Two vaccinations": 0.1,
+          "Three vaccinations": 0.5,
+          "Four or more vaccinations": 0.1,
         }
       },
     },
